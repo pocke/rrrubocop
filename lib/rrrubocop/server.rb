65f97fa37ce0b40fe27039fc0509187ce923faea
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-09-04 21:44:43 +0900

    WIP server

--- lib/rrrubocop/server.rb
+++ lib/rrrubocop/server.rb
@@ -1,9 +1,17 @@
+require 'thread'
+require 'json'
+require 'socket'
+require 'rbconfig'
+
 module RRRuboCop
   class Server
+    Request = Struct.new("Request", :id, :body)
+
+    # TODO: get file paths
     def run
-      # TODO: spawn with current ruby path
-      pid = Process.spawn(client_path)
-      Process.wait(pid)
+      port = start_server(req_ch, resp_ch)
+      start_workers(port)
+      resp.ch.wait
     end
 
 
@@ -11,6 +19,41 @@ module RRRuboCop
 
     def client_path
       File.join(RRRuboCop::RootDir, 'exe', 'rrrubocop-client')
+    end
+
+
+    # @param req_ch [Thread::Queue<RRRuboCop::Server::Request>]
+    # @param resp_ch [Thread::Queue<RRRuboCop::Server::Response>]
+    # @return [Integer] TCP port number
+    def start_server(req_ch, resp_ch)
+      server = TCPServer.new(0)
+
+      Thread.new do
+        loop do
+          client = server.accept
+          req = req_ch.pop
+          break unless req # queue is closed
+
+          client.puts JSON.parse(req.body)
+          client.flush
+          resp_raw = JSON.parse(client.read)
+          resp = Response.new(resp_raw)
+          resp.id = req.id
+          resp_ch.push resp
+          client.close
+        end
+      end
+
+      return server.addr[1]
+    end
+
+    # @param port [Integer] TCP Port number of RRRuboCop server
+    def start_workers(port)
+      # FIXME
+      nproc = `nproc`.to_i
+      nproc.times do
+        Process.spawn(RbConfig.ruby, client_path, '--port', port.to_s)
+      end
     end
   end
 end
