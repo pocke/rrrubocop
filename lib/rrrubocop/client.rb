54efe5e1a5a949a8c4f077d24c27f4774e7ba689
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-09-04 18:28:59 +0900

    WIP

--- lib/rrrubocop/client.rb
+++ lib/rrrubocop/client.rb
@@ -1,7 +1,21 @@
 module RRRuboCop
   class Client
+    EndOfFiles = Class.new(StandardError)
+
     def run
-      sleep 1
+      loop do
+        args = fetch_cli_args
+        cli = RuboCop::CLI.new
+        cli.run(args)
+      end
+    rescue EndOfFiles
+      # Analysing finish.
+    end
+
+
+    private
+
+    def fetch_cli_args
     end
   end
 end
