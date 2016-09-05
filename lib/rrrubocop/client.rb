fea2330ee9eab71151b9dca0ce78a7a66e525f24
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-09-05 22:29:03 +0900

    WIP client

--- lib/rrrubocop/client.rb
+++ lib/rrrubocop/client.rb
@@ -1,6 +1,16 @@
+require 'socket'
+require 'json'
+
 module RRRuboCop
   class Client
     EndOfFiles = Class.new(StandardError)
+
+    # @param args [Array<String>] command line arguments
+    def initialize(args)
+      # FIXME
+      @port = args[1].to_i
+      @host = "127.0.0.1"
+    end
 
     def run
       loop do
@@ -10,12 +20,26 @@ module RRRuboCop
       end
     rescue EndOfFiles
       # Analysing finish.
+      exit 0
     end
 
 
     private
 
     def fetch_cli_args
+      resp = fetch
+      resp['arguments']
+    end
+
+    def fetch
+      # https://github.com/pocke/rrrubocop/issues/1
+      s = TCPSocket.open(@host, @port)
+      raw_data = s.gets
+      data = JSON.parse(raw_data)
+      raise EndOfFiles, data['error'] if data['error']
+      return data
+    ensure
+      s.close
     end
   end
 end
